bdf5dc1aa1071add778bc786c590a2a78f6f4f47
keik <k4t0.kei@gmail.com>
2016-03-08 18:06:02 +0900

    Add tests

--- test/test.js
+++ test/test.js
@@ -21,6 +21,11 @@ test('single line-preserved-transform on 1.js', function(t) {
 
   // exercise
   var transformed = tr_doubleId(code, f)
+  t.equal(convert.removeComments(transformed),
+          [
+            'aa = bb;',
+            ''
+          ].join('\n'))
 
   // verify
   var map = convert.fromSource(transformed).toJSON()
@@ -43,6 +48,12 @@ test('single line-breaking-transform on 1.js', function(t) {
 
   // exercise
   var transformed = tr_doubleLine(code, f)
+  t.equal(convert.removeComments(transformed),
+          [
+            'a = b;',
+            'a = b;',
+            ''
+          ].join('\n'))
 
   // verify
   var map = convert.fromSource(transformed).toJSON()
@@ -57,6 +68,91 @@ test('single line-breaking-transform on 1.js', function(t) {
   t.equal(origPos.line, 1)
   t.equal(origPos.column, 0)
   origPos = con.originalPositionFor({line: 2, column: 4})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 4)
+  t.end()
+  // con.eachMapping(e => console.log(e))
+  // console.log(transformed);
+})
+
+test('multi line-preserved-transform on 1.js', function(t) {
+  // setup
+  var f = '1.js'
+  var code = codes[f]
+
+  // exercise
+  // x3
+  code = tr_doubleId(code, f)
+  code = tr_doubleId(code, f)
+  var transformed = tr_doubleId(code, f)
+  t.equal(convert.removeComments(transformed),
+          [
+            'aaaaaaaa = bbbbbbbb;',
+            ''
+          ].join('\n'))
+
+  // verify
+  var map = convert.fromSource(transformed).toJSON()
+  var con = new SourceMapConsumer(map)
+  var origPos = con.originalPositionFor({line: 1, column: 0})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 0)
+  origPos = con.originalPositionFor({line: 1, column: 11})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 4)
+  t.end()
+  // con.eachMapping(e => console.log(e))
+  // console.log(transformed);
+})
+
+test('multi line-breaking-transform on 1.js', function(t) {
+  // setup
+  var f = '1.js'
+  var code = codes[f]
+
+  // exercise
+  // x3
+  code = tr_doubleLine(code, f)
+  code = tr_doubleLine(code, f)
+  var transformed = tr_doubleLine(code, f)
+  t.equal(convert.removeComments(transformed),
+          [
+            'a = b;',
+            'a = b;',
+            'a = b;',
+            'a = b;',
+            'a = b;',
+            'a = b;',
+            'a = b;',
+            'a = b;',
+            ''
+          ].join('\n'))
+
+  // verify
+  var map = convert.fromSource(transformed).toJSON()
+  var con = new SourceMapConsumer(map)
+  var origPos = con.originalPositionFor({line: 1, column: 0})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 0)
+  origPos = con.originalPositionFor({line: 1, column: 4})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 4)
+  origPos = con.originalPositionFor({line: 2, column: 0})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 0)
+  origPos = con.originalPositionFor({line: 2, column: 4})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 4)
+  origPos = con.originalPositionFor({line: 3, column: 0})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 0)
+  origPos = con.originalPositionFor({line: 3, column: 4})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 4)
+  origPos = con.originalPositionFor({line: 4, column: 0})
+  t.equal(origPos.line, 1)
+  t.equal(origPos.column, 0)
+  origPos = con.originalPositionFor({line: 4, column: 4})
   t.equal(origPos.line, 1)
   t.equal(origPos.column, 4)
   t.end()
