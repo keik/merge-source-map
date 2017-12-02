2f7eceb6dfe224649455c014155f657458c06e8a
keik <keik@c-fo.com>
2017-12-02 22:40:50 +0900

    Apply prettier

--- index.js
+++ index.js
@@ -13,11 +13,8 @@ module.exports = merge
  * @return {object|undefined} merged source map object, or undefined when both old and new source map are undefined
  */
 function merge(oldMap, newMap) {
-
-  if (!oldMap)
-    return newMap
-  if (!newMap)
-    return oldMap
+  if (!oldMap) return newMap
+  if (!newMap) return oldMap
 
   var oldMapConsumer = new SourceMapConsumer(oldMap)
   var newMapConsumer = new SourceMapConsumer(newMap)
@@ -25,15 +22,16 @@ function merge(oldMap, newMap) {
 
   // iterate on new map and overwrite original position of new map with one of old map
   newMapConsumer.eachMapping(function(m) {
-
     // pass when `originalLine` is null.
     // It occurs in case that the node does not have origin in original code.
-    if (m.originalLine == null)
-      return
+    if (m.originalLine == null) return
 
-    var origPosInOldMap = oldMapConsumer.originalPositionFor({line: m.originalLine, column: m.originalColumn})
-    if (origPosInOldMap.source == null)
-      return
+    var origPosInOldMap = oldMapConsumer.originalPositionFor({
+      line: m.originalLine,
+      column: m.originalColumn
+    })
+
+    if (origPosInOldMap.source == null) return
 
     mergedMapGenerator.addMapping({
       original: {
@@ -51,7 +49,7 @@ function merge(oldMap, newMap) {
 
   var mergedMap = JSON.parse(mergedMapGenerator.toString())
 
-  mergedMap.sourcesContent = mergedMap.sources.map(function (source) {
+  mergedMap.sourcesContent = mergedMap.sources.map(function(source) {
     return oldMapConsumer.sourceContentFor(source)
   })
 
